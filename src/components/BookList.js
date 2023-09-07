07b40a81a9a5a7606c891f6698efe93cf0361732
Yen Tran <yentran@Yens-MBP.hawaiiantel.net>
2023-09-06 16:22:48 -1000

    first version

---
+++ src/components/BookList.js
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import BooksContext from '../context/books';
+import BookShow from './BookShow';
+
+function BookList({ books, onDelete, onEdit }) {
+  const { count, incrementCount } = useContext(BooksContext);
+
+  const renderedBooks = books.map((book) => {
+    return <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />;
+  });
+  return (
+    <div className="book-list">
+      {count}
+      <button onClick={incrementCount}>Click</button>
+      {renderedBooks}
+    </div>
+  );
+}
+
+export default BookList;
